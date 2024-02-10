8487dfff782972b0733379abf07b8d95c6135c39
Kirill Mikhailov <kicode@mail.ru>
2024-02-10 20:16:42 +0300

    fix

--- handlers/user/main_router.py
+++ handlers/user/main_router.py
@@ -6,10 +6,12 @@ from .get_detail import detail_router
 
 from middlewares import CheckUser
 
+from filters import ChatTypeFilter
+
 main_router_user = Router()
 
 start_router.message.middleware(CheckUser())
-
+start_router.message.filter(ChatTypeFilter(chat_type=["group", "supergroup"]))
 main_router_user.include_routers(
     start_router,
     terms_router,
