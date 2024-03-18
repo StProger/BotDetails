d583623162159e96b80c76269f03d454e32b1d4c
Kirill Mikhailov <kicode@mail.ru>
2024-03-18 19:25:49 +0300

    add sent update to leadtech

--- config.py
+++ config.py
@@ -12,3 +12,5 @@ TOKEN_BOT = os.getenv("TOKEN_BOT")
 DIRECTUS_API_URL = os.getenv("DIRECTUS_API_URL")
 
 REDIS_URL = os.getenv("REDIS_URL")
+
+URL_UPDATE="https://b309120.leadteh.ru/webhooks/telegram/293619:a6T8ULYyV6dweMtF81iubXJOi9Z84uwzU2fSf4Rpdt45QWGBSYWKEIzjSUtnHB0C"
---
+++ utils/sent_update.py
@@ -0,0 +1,12 @@
+from config import URL_UPDATE
+
+import aiohttp
+
+
+async def sent_update_leadtech(body):
+
+    url = URL_UPDATE
+
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(10)) as session:
+        response = await session.post(url, json=body)
+    print(response.status)
